f1793170c18c681271e632279a70a78e6d54abaf
Ben Hoskings <ben@hoskings.net>
2011-05-28 20:43:47 +1000

    Renamed SourcePool::SOURCE_DEP_SEPARATOR to SEPARATOR.

--- lib/babushka/source_pool.rb
+++ lib/babushka/source_pool.rb
@@ -2,7 +2,7 @@ module Babushka
   class SourcePool
     include Singleton
 
-    SOURCE_DEP_SEPARATOR = ':'
+    SEPARATOR = ':'
 
     def current
       @_cached_current ||= default.concat(standard)
@@ -45,8 +45,8 @@ module Babushka
     def dep_for dep_spec, opts = {}
       if dep_spec.is_a?(Dep)
         dep_spec
-      elsif dep_spec[/#{SOURCE_DEP_SEPARATOR}/] # If a source was specified, that's where we load from.
-        source_name, dep_name = dep_spec.split(SOURCE_DEP_SEPARATOR, 2)
+      elsif dep_spec[/#{SEPARATOR}/] # If a source was specified, that's where we load from.
+        source_name, dep_name = dep_spec.split(SEPARATOR, 2)
         source_for(source_name).find(dep_name)
       elsif opts[:from]
         opts[:from].find(dep_spec) || dep_for(dep_spec)
@@ -63,8 +63,8 @@ module Babushka
     def template_for template_spec, opts = {}
       if template_spec.nil?
         nil
-      elsif template_spec[/#{SOURCE_DEP_SEPARATOR}/] # If a source was specified, that's where we load from.
-        source_name, template_name = template_spec.split(SOURCE_DEP_SEPARATOR, 2)
+      elsif template_spec[/#{SEPARATOR}/] # If a source was specified, that's where we load from.
+        source_name, template_name = template_spec.split(SEPARATOR, 2)
         source_for(source_name).find_template(template_name)
       elsif opts[:from]
         opts[:from].find_template(template_spec) || template_for(template_spec)
